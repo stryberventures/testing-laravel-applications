bdf10b02954ffc1946635094ba9e9b4b0cd4569e
slia-stryber <Serhii Liamin>
2022-11-03 16:53:11 +0200

    data providers with closure + saved configuration

--- tests/Feature/DataProviders/AddTest.php
+++ tests/Feature/DataProviders/AddTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\DataProviders;
 
+use Closure;
+use DateTime;
 use Tests\TestCase;
 
 final class AddTest extends TestCase
@@ -16,6 +18,7 @@ final class AddTest extends TestCase
         $this->assertSame($expected, $a + $b);
     }
 
+    // Simple Data Provider
     public function providerAddSuccess(): array
     {
         return [
@@ -37,6 +40,7 @@ final class AddTest extends TestCase
         $this->assertSame($expected, $a + $b);
     }
 
+    // Data Provider with yield
     public function providerAddSuccessIterated(): iterable
     {
         /* [$firstArgument, $secondArgument, $expectedResult] */
@@ -44,5 +48,34 @@ final class AddTest extends TestCase
         yield 'add 0 and 1' => [0, 1, 1];
         yield 'add 1 and 0' => [1, 0, 1];
         yield 'add 1 and 1' => [1, 1, 3];
+    }
+
+    // =================================================================================================================
+
+    /**
+     * @dataProvider providerAddSuccessClosure
+     */
+    public function testAddWithClosureDataProvider(string $expectedResult, Closure $setTime): void
+    {
+        $dateTime = new DateTime('2019-09-01');
+
+        // pass reference to the current class to have access to the class properties/methods
+        $setTime($this, $dateTime);
+
+        self::assertSame($expectedResult, $dateTime->format('Y-m-d H:i:s'));
+    }
+
+    // Data Provider with closure
+    public function providerAddSuccessClosure(): iterable
+    {
+        yield 'midnight' => [
+            '2019-09-01 00:00:00',
+            function (AddTest $self, DateTime $date): void {
+                // do something with current test instance
+                // $self->user->id
+
+                $date->setTime(0, 0, 0);
+            },
+        ];
     }
 }
